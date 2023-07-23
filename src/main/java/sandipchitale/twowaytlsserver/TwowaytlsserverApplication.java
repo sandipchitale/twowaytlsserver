c70325856aaf935e89f8e55114b542814dd55905
SANDIP CHITALE <sandipchitale@gmail.com>
2023-07-22 21:01:02 -0700

    ConfigurationPropertiesBindingPostProcessor. Update response.

--- src/main/java/sandipchitale/twowaytlsserver/TwowaytlsserverApplication.java
+++ src/main/java/sandipchitale/twowaytlsserver/TwowaytlsserverApplication.java
@@ -1,7 +1,13 @@
 package sandipchitale.twowaytlsserver;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBean;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +18,30 @@ public class TwowaytlsserverApplication {
 	public static class IndexController {
 		@GetMapping("/")
 		public String index() {
-			return "Hello World!";
+			return "Hello from Server!";
+		}
+	}
+
+	@Component
+	public static class CPPP extends ConfigurationPropertiesBindingPostProcessor {
+
+		private ApplicationContext applicationContext;
+
+		@Override
+		public void setApplicationContext(ApplicationContext applicationContext) {
+			super.setApplicationContext(applicationContext);
+			this.applicationContext = applicationContext;
+		}
+
+		@Override
+		public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+			// Only if this is  ConfigurationProperties bean
+			if (ConfigurationPropertiesBean.get(applicationContext, bean, beanName) != null) {
+				if (bean instanceof ServerProperties serverProperties) {
+					return bean;
+				}
+			}
+			return super.postProcessAfterInitialization(bean, beanName);
 		}
 	}
 
