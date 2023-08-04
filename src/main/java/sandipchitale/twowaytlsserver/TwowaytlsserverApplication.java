c0e20d54ef95f96288dac4f60c13bb2a3c5399aa
SANDIP CHITALE <sandipchitale@gmail.com>
2023-08-03 17:45:10 -0700

    Remove ConfigurationPropertiesBindingPostProcessor.

--- src/main/java/sandipchitale/twowaytlsserver/TwowaytlsserverApplication.java
+++ src/main/java/sandipchitale/twowaytlsserver/TwowaytlsserverApplication.java
@@ -16,29 +16,6 @@ public class TwowaytlsserverApplication {
 		}
 	}
 
-//	@Component
-//	public static class CPPP extends ConfigurationPropertiesBindingPostProcessor {
-//
-//		private ApplicationContext applicationContext;
-//
-//		@Override
-//		public void setApplicationContext(ApplicationContext applicationContext) {
-//			super.setApplicationContext(applicationContext);
-//			this.applicationContext = applicationContext;
-//		}
-//
-//		@Override
-//		public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-//			// Only if this is ConfigurationProperties bean
-//			if (ConfigurationPropertiesBean.get(applicationContext, bean, beanName) != null) {
-//				if (bean instanceof ServerProperties serverProperties) {
-//					return bean;
-//				}
-//			}
-//			return super.postProcessAfterInitialization(bean, beanName);
-//		}
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TwowaytlsserverApplication.class, args);
 	}
